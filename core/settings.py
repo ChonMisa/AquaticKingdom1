3cb472225a7a69fe5ab1e0e282a6f80c1c7eedea
Your Name <you@example.com>
2024-06-12 14:53:45 +0600

    Added new features and fixed bugs

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_DIR = BASE_DIR / 'static'
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
