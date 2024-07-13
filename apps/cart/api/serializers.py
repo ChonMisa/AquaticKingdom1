e741c19b664e9bd9c2335cf7dd31474d36f9d2c1
ℳisa <152699938+ChonMisa@users.noreply.github.com>
2024-07-13 16:26:53 +0600

    Update serializers.py

--- apps/cart/api/serializers.py
+++ apps/cart/api/serializers.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
-from apps.cart.models import Cart, Item
-from apps.fish.models import Fish
-from apps.accessories.models import Accessory
-from apps.fish_food.models import FishFood
+
+from apps.cart.models import Cart, Item, Order
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ['id', 'fish', 'accessory', 'ffood', 'quantity', 'cart']
+        read_only_fields = ['id', 'cart']
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -18,3 +17,11 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ['id', 'user', 'items_cart', 'total_sum']
+        read_only_fields = ['id', 'user']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'items', 'created_at', 'updated_at', 'status', 'delivery_type']
+        read_only_fields = ['id', 'user', 'created_at', 'updated_at', 'status']
