e4be60b69c0d45b2ac55605ffbcb9da458ba2ca9
Your Name <you@example.com>
2024-07-21 19:19:42 +0600

    были исключены stock and restock

--- apps/fish_food/admin.py
+++ apps/fish_food/admin.py
@@ -6,6 +6,6 @@ from apps.fish_food.models import FishFood
 @admin.register(FishFood)
 class FishFoodAdmin(admin.ModelAdmin):
     list_display = ['title', 'price']
-    list_filter = ['title', 'restock_date']
+    list_filter = ['title']
     search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
